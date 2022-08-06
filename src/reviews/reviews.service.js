05ccb2f86b0de4f1bbb050b6022b85a2994395da
Arjay <95892997+arjaydiala@users.noreply.github.com>
2022-08-06 11:45:14 -0700

    Add files via upload

---
+++ src/reviews/reviews.service.js
@@ -0,0 +1,57 @@
+
+
+const knex = require("../db/connection");
+
+/**
+ * @param {integer} movie_id 
+ * @returns reviews matching movie_id
+ */
+function list(movie_id) {
+  return knex("reviews").select("*").where({ movie_id });
+}
+
+/**
+ * @param {integer} review_id 
+ * @returns review matching review_id
+ */
+function read(review_id) {
+  return knex("reviews").select("*").where({ review_id }).first();
+}
+
+/**
+ * updates review with matching review_id
+ * @param {text} updatedReview 
+ */
+function update(updatedReview) {
+  return knex("reviews")
+    .select("*")
+    .where({ review_id: updatedReview.review_id })
+    .update(updatedReview, "*");
+}
+
+/**
+ * @param {integer} critic_id 
+ * @returns critic matching critic_id
+ */
+function readCritic(critic_id) {
+  return knex("critics")
+    .select("*")
+    .where({ critic_id })
+    .first();
+}
+
+/**
+ * deletes review with matching review_id
+ * @param {integer} review_id  
+ */
+function destroy(review_id) {
+  return knex("reviews").where({ review_id }).del();
+}
+
+module.exports = {
+  list,
+  read,
+  update,
+  readCritic,
+  delete: destroy,
+};
